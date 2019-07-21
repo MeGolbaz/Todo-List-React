78a0f0307d346bf6c43db25779a04fe5a63c2b5e
Mehdi Golbaz <mehdigolbaz@Mehdis-MacBook-Pro.local>
2019-07-21 20:35:33 +0430

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [newTodo, setNewTodo] = useState("")
+
+  const [todos, setTodos] = useState([
+    { name: "JS Practicing", isDone: false } , 
+    { name: "Going Gym", isDone: false },
+  ])
+
+  const handleOnchange = e => {
+    setNewTodo(e.target.value)
+  }
+
+  const handleOnClick = () => {
+    setTodos([...todos, { name: newTodo, isDone: false }])
+    setNewTodo("")
+  }
+
+  const handleDelete = index => {
+    setTodos(todos.filter((todo, i) => i != index))
+  }
+
+  const handleDone = index => {
+    setTodos(todos.map((todo,i) => i === index ? {...todo, isDone: !todo.isDone } : todo))
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div id="myDIV" class="header">
+        <h2>My To Do List</h2>
+        <input type="text" value={newTodo} onChange={handleOnchange} placeholder="Title..." />
+        <button onClick={handleOnClick} disabled={newTodo == ""} class="addBtn">Add</button>
     </div>
+    
+    <ul id="myUL">
+      {todos.map((todo, index) => <li onClick={()=>handleDone(index)} class={todo.isDone ? 'checked' : ''}><span onClick={() => handleDelete(index)} class="close" >×</span>{todo.name}</li>)}
+        
+        {/* <li class="checked">Going Gym</li> */}
+    </ul>  
+    </>
   );
 }
 
 export default App;
+
